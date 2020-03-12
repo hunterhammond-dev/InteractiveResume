e096ae197d4e123c4384e9d5ad055c1d87988386
hunterhammond-dev <59210517+hunterhammond-dev@users.noreply.github.com>
2020-03-12 15:25:15 -0500

    Made the about section of the page into a component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
